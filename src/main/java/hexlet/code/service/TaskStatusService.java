b40273078baa90c3081115c5d8f5f653e7f5c545
Julia Kon <kon1976y@gmail.com>
2025-10-21 16:40:41 +0300

    add check exist status

--- src/main/java/hexlet/code/service/TaskStatusService.java
+++ src/main/java/hexlet/code/service/TaskStatusService.java
@@ -33,6 +33,9 @@ public class TaskStatusService {
     }
 
     public TaskStatusDTO createStatus(TaskStatusCreateDTO data) {
+        if (repository.findBySlug(data.getSlug()).isPresent()) {
+            throw new RuntimeException("Status already exists");
+        }
         var status = mapper.map(data);
         repository.save(status);
         return mapper.map(status);
