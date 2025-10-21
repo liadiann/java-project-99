db465d962b45ddde17af827363b0688108267623
Julia Kon <kon1976y@gmail.com>
2025-10-21 15:56:48 +0300

    fix Datainitializer

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -2,6 +2,8 @@ package hexlet.code.component;
 
 import hexlet.code.dto.TaskStatusCreateDTO;
 import hexlet.code.dto.UserCreateDTO;
+import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
 import hexlet.code.service.TaskStatusService;
 import hexlet.code.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +17,34 @@ public class DataInitializer implements ApplicationRunner {
     private UserService userService;
     @Autowired
     private TaskStatusService taskStatusService;
+    @Autowired
+    private TaskStatusRepository repository;
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public void run(ApplicationArguments args) throws Exception {
         var email = "hexlet@example.com";
-        var password = "qwerty";
-        var userDto = new UserCreateDTO();
-        userDto.setEmail(email);
-        userDto.setPassword(password);
-        userService.createUser(userDto);
-
-        taskStatusService.createStatus(createTaskStatusCreateDTO("Draft", "draft"));
-        taskStatusService.createStatus(createTaskStatusCreateDTO("ToReview", "to_review"));
-        taskStatusService.createStatus(createTaskStatusCreateDTO("ToBeFixed", "to_be_fixed"));
-        taskStatusService.createStatus(createTaskStatusCreateDTO("ToPublish", " to_publish"));
-        taskStatusService.createStatus(createTaskStatusCreateDTO("Published", "published"));
+        if (userRepository.findByEmail(email).isEmpty()) {
+            var password = "qwerty";
+            var userDto = new UserCreateDTO();
+            userDto.setEmail(email);
+            userDto.setPassword(password);
+            userService.createUser(userDto);
+        }
+        String[][] statuses = {
+                {"Draft", "draft"},
+                {"ToReview", "to_review"},
+                {"ToBeFixed", "to_be_fixed"},
+                {"ToPublish", " to_publish"},
+                {"Published", "published"}
+        };
+        for (var status: statuses) {
+            var name = status[0];
+            var slug = status[1];
+            if (repository.findBySlug(slug).isEmpty()) {
+                taskStatusService.createStatus(createTaskStatusCreateDTO(name, slug));
+            }
+        }
     }
 
     public TaskStatusCreateDTO createTaskStatusCreateDTO(String name, String slug) {
