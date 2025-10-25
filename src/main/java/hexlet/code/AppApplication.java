316269595dd613bf24388bedfa3ad16a760492fc
Julia Kon <kon1976y@gmail.com>
2025-10-25 17:52:18 +0300

    fix

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,6 +1,5 @@
 package hexlet.code;
 
-import io.sentry.Sentry;
 import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
